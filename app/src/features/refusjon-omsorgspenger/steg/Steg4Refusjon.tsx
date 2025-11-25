215be46346929be1c3afc984de0717415399ddac
vebnor <vebjorn.nordby@nav.no>
2025-11-25 16:18:52 +0100

    Fjerner alert for refusjon-omsorgspenger. Kan vises på samme sted som for vanlig inntektsmelding

--- app/src/features/refusjon-omsorgspenger/steg/Steg4Refusjon.tsx
+++ app/src/features/refusjon-omsorgspenger/steg/Steg4Refusjon.tsx
@@ -145,7 +145,6 @@ export const RefusjonOmsorgspengerArbeidsgiverSteg4 = () => {
               </Button>
             </>
           ) : null}
-          <InntektAlert />
           <div className="flex gap-4">
             <Button
               as={Link}
@@ -189,20 +188,6 @@ function Over6GAlert() {
       <Alert variant="info">
         Nav utbetaler opptil 6G av årslønnen. Du skal likevel føre opp den
         lønnen dere utbetaler til den ansatte i sin helhet.
-      </Alert>
-    );
-  }
-  return null;
-}
-
-function InntektAlert() {
-  const { formState } = useFormContext<RefusjonOmsorgspengerFormData>();
-  const error = formState.errors.inntekt;
-
-  if (error) {
-    return (
-      <Alert variant="error">
-        <BodyLong>{error.message}</BodyLong>
       </Alert>
     );
   }
