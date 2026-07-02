48f95221c6bd9ff41add22d44117466012b36beb
vebnor <vebjorn.nordby@nav.no>
2026-07-02 11:08:55 +0200

    bytter endepunkt for refusjon

--- app/src/features/refusjon-omsorgspenger/steg/Steg3Omsorgsdager.tsx
+++ app/src/features/refusjon-omsorgspenger/steg/Steg3Omsorgsdager.tsx
@@ -68,7 +68,7 @@ export const RefusjonOmsorgspengerArbeidsgiverSteg3 = () => {
   const { data: inntektsmeldingerForÅr } = useHentInntektsmeldingForÅr({
     aktørId: watch("ansattesAktørId") as string,
     arbeidsgiverIdent: watch("organisasjonsnummer") as string,
-    år: årForRefusjon,
+    år: Number(årForRefusjon),
   });
 
   useEffect(() => {
